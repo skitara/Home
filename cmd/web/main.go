99c8bc7e21c58b5c747a70b9e8e2cd3751d300ad
Skitara <AlexeyOsipenko@proton.me>
2024-12-13 17:01:30 +0200

    feat: add method-specific validation for HTTP handlers

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,27 +5,56 @@ import (
 	"net/http"
 )
 
-// handler for logic
-func index(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodGet)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	w.Write([]byte("Welcome to my blog!"))
 }
 
 func createPost(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		// If it's not, use the w.WriteHeader() method to send a 405 status
+		// code and the w.Write() method to write a "Method Not Allowed"
+		// response body. We then return from the function so that the
+		// subsequent code is not executed.
+		w.Header().Set("Allow", http.MethodPost)
+		//w.WriteHeader(405)
+		//w.Write([]byte("Method Not Allowed"))
+		http.Error(w, "Method Now Allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	w.Write([]byte("Create post!"))
 }
 
 func changePost(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		// w.WriteHeader(405)
+		// w.Write([]byte("Method Not Allowed"))
+		w.Header().Set("Allow", http.MethodPut)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	w.Write([]byte("Change post!"))
 }
 
 func deletePost(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodDelete {
+		// w.WriteHeader(405)
+		// w.Write([]byte("Method Not Allowed"))
+		w.Header().Set("Allow", http.MethodDelete)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	w.Write([]byte("Delete post"))
 }
 
 func main() {
 	// servemux for routing
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", index)
+	mux.HandleFunc("/", home)
 	mux.HandleFunc("/create", createPost)
 	mux.HandleFunc("/change", changePost)
 	mux.HandleFunc("/delete", deletePost)
